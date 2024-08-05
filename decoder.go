d219ce9d7168facce5c68024668d253045eed770
Jille Timmermans <jille@hexon.nl>
2024-08-05 16:26:06 +0200

    SplitMap splits a msgpack map into string-keys and the msgpack-values.
    It does not decode the values.

--- decoder.go
+++ decoder.go
@@ -529,3 +529,41 @@ func SplitArray(data []byte) ([][]byte, error) {
 	}
 	return ret, nil
 }
+
+// SplitMap splits a msgpack map into string-keys and the msgpack-values. It does not decode the values.
+// The returned slices point into the given data.
+func SplitMap(data []byte, dict *Dict) ([]string, [][]byte, error) {
+	data = data[internal.DecodeLengthPrefixExtension(data):]
+	elements, consume, ok := internal.DecodeMapLen(data)
+	if !ok {
+		return nil, nil, fmt.Errorf("encountered msgpack byte %02x while expecting a map at offset %d", data[0], 0)
+	}
+	keys := make([]string, elements)
+	values := make([][]byte, elements)
+	rc := resolveCall{
+		dict:   dict,
+		data:   data,
+		offset: consume,
+	}
+	for i := 0; elements > i; i++ {
+		kv := rc.resolveValue()
+		if rc.err != nil {
+			return nil, nil, rc.err
+		}
+		switch kv := kv.(type) {
+		case string:
+			keys[i] = kv
+		case []byte:
+			keys[i] = unsafeslice.StringFromByteSlice(kv)
+		default:
+			return nil, nil, fmt.Errorf("fastmsgpack doesn't support non-string keys in maps (like %T)", kv)
+		}
+		start := rc.offset
+		rc.skipValue()
+		if rc.err != nil {
+			return nil, nil, rc.err
+		}
+		values[i] = data[start:rc.offset]
+	}
+	return keys, values, nil
+}
