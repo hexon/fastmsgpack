2459b67dfa04e4e544204479d490c6d59069e811
Jille Timmermans <jille@hexon.nl>
2025-09-23 13:22:51 +0200

    FlavorBuilder: Add AppendMsgpack method
    More efficient when building large outputs

--- flavorbuilder.go
+++ flavorbuilder.go
@@ -3,6 +3,9 @@ package fastmsgpack
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
+	"math"
+	"slices"
 
 	"github.com/dennwc/varint"
 )
@@ -48,11 +51,44 @@ func (f *FlavorBuilder) dataChunk(b []byte) int {
 	return len(f.dataChunks) - 1
 }
 
-func (f FlavorBuilder) MarshalMsgpack() ([]byte, error) {
-	if len(f.dataChunks) == 1 {
-		return f.dataChunks[0], nil
+func (f FlavorBuilder) extensionHeader(size int) ([6]byte, int, error) {
+	var ret [6]byte
+	if size > math.MaxUint32 {
+		return ret, -1, errors.New("too big for a msgpack extension to encode")
+	} else if size > math.MaxUint16 {
+		ret[0] = 0xc9
+		binary.BigEndian.PutUint32(ret[1:5], uint32(size))
+		ret[5] = 18
+		return ret, 6, nil
+	} else if size > math.MaxUint8 {
+		ret[0] = 0xc8
+		binary.BigEndian.PutUint16(ret[1:3], uint16(size))
+		ret[3] = 18
+		return ret, 4, nil
+	} else {
+		switch size {
+		case 1:
+			ret[0] = 0xd4
+		case 2:
+			ret[0] = 0xd5
+		case 4:
+			ret[0] = 0xd6
+		case 8:
+			ret[0] = 0xd7
+		case 16:
+			ret[0] = 0xd8
+		default:
+			ret[0] = 0xc7
+			ret[1] = byte(size)
+			ret[2] = 18
+			return ret, 3, nil
+		}
+		ret[1] = 18
+		return ret, 2, nil
 	}
+}
 
+func (f FlavorBuilder) flavorHeader(buf []byte) []byte {
 	var jumpOffset int
 	chunkOffsets := make([]uint64, len(f.dataChunks))
 	for i, b := range f.dataChunks {
@@ -60,7 +96,8 @@ func (f FlavorBuilder) MarshalMsgpack() ([]byte, error) {
 		jumpOffset += len(b)
 	}
 
-	buf := make([]byte, 0, binary.MaxVarintLen64*(2+2*len(f.cases))+jumpOffset)
+	buf = slices.Grow(buf, binary.MaxVarintLen64*(2+2*len(f.cases))+jumpOffset)
+	lengthAtStart := len(buf)
 	buf = binary.AppendUvarint(buf, uint64(f.field))
 	numCases := len(f.cases) << 1
 	if f.elseCase != nil {
@@ -69,7 +106,7 @@ func (f FlavorBuilder) MarshalMsgpack() ([]byte, error) {
 	buf = binary.AppendUvarint(buf, uint64(numCases))
 	staticHeader := buf
 
-	headerLen := len(buf)
+	headerLen := len(buf) - lengthAtStart
 	for _, c := range f.cases {
 		headerLen += varint.UvarintSize(uint64(c.match))
 		headerLen += varint.UvarintSize(chunkOffsets[c.chunk])
@@ -86,16 +123,75 @@ func (f FlavorBuilder) MarshalMsgpack() ([]byte, error) {
 		if f.elseCase != nil {
 			buf = binary.AppendUvarint(buf, uint64(headerLen)+chunkOffsets[*f.elseCase])
 		}
-		if len(buf) <= headerLen {
-			clear(buf[len(buf):headerLen])
-			buf = buf[:headerLen]
+		if len(buf) <= headerLen+lengthAtStart {
+			buf = buf[:lengthAtStart+headerLen]
 			break
 		}
-		headerLen = len(buf)
+		headerLen = len(buf) - lengthAtStart
 	}
+	return buf
+}
+
+func (f FlavorBuilder) AppendMsgpack(dst []byte) ([]byte, error) {
+	if len(f.dataChunks) == 1 {
+		return append(dst, f.dataChunks[0]...), nil
+	}
+	dataSize := 0
+	for _, b := range f.dataChunks {
+		dataSize += len(b)
+	}
+	var estimatedHeaderSize int
+	// Reserve space for the extension header.
+	if dataSize > math.MaxUint16 {
+		dst = append(dst, 0xc1, 0xc1, 0xc1, 0xc1, 0xc1, 0xc1)
+		estimatedHeaderSize = 6
+	} else {
+		// Most likely it's 3 or 4 bytes.
+		dst = append(dst, 0xc1, 0xc1, 0xc1, 0xc1)
+		estimatedHeaderSize = 4
+	}
+	lengthBefore := len(dst)
+	dst = f.flavorHeader(dst)
+	size := len(dst) - lengthBefore + dataSize
+	h, headerSize, err := f.extensionHeader(size)
+	if err != nil {
+		return nil, err
+	}
+	if headerSize > estimatedHeaderSize {
+		// We estimated the extension header too small and need to move the flavor header down a little.
+		dst = dst[len(dst)+headerSize-estimatedHeaderSize:]
+		copy(dst[lengthBefore+headerSize-estimatedHeaderSize:], dst[lengthBefore:])
+	} else if headerSize < estimatedHeaderSize {
+		copy(dst[lengthBefore+headerSize-estimatedHeaderSize:], dst[lengthBefore:])
+		dst = dst[:len(dst)+headerSize-estimatedHeaderSize]
+	}
+	copy(dst[lengthBefore-estimatedHeaderSize:], h[:headerSize])
+	for _, b := range f.dataChunks {
+		dst = append(dst, b...)
+	}
+	return dst, nil
+}
+
+var sixZeroes = make([]byte, 6, 6)
+
+func (f FlavorBuilder) MarshalMsgpack() ([]byte, error) {
+	if len(f.dataChunks) == 1 {
+		return f.dataChunks[0], nil
+	}
+	dataSize := 0
+	for _, b := range f.dataChunks {
+		dataSize += len(b)
+	}
+	buf := f.flavorHeader(sixZeroes)
+	size := len(buf) - 6 + dataSize
+	h, headerSize, err := f.extensionHeader(size)
+	if err != nil {
+		return nil, err
+	}
+	buf = buf[6-headerSize:]
+	copy(buf, h[:headerSize])
 	for _, b := range f.dataChunks {
 		buf = append(buf, b...)
 	}
-
-	return Extension{Type: 18, Data: buf}.MarshalMsgpack()
+	return buf, nil
 }
